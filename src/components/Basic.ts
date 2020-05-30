350ed0fa9a8de6ac386b2a05ad7763d4f29807b8
Joao Victor Cabral de Melo <JoaoVictorCabraldeMelo>
2020-05-30 19:37:04 -0300

    :tada: First Commit

---
+++ src/components/Basic.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+export const Background = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #ae34ae34; 
+`
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Background } from "../components/Basic";
+
+
+const Home = props => {
+  return(
+    <Background />
+  )
+}
+
+export default Home;
